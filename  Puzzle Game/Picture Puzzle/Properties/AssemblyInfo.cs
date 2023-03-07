fd38e12dafadd1907364e742cd54c89bde136034
Marouan <115103579+marouanal@users.noreply.github.com>
2023-03-07 18:05:19 +0000

    Add files via upload

---
+++  Puzzle Game/Picture Puzzle/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Picture Puzzle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Picture Puzzle")]
+[assembly: AssemblyCopyright("Copyright ©  M/A")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6fa34fd4-8756-49a0-8da3-b5f76dbe51e9")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
